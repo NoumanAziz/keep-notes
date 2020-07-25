ac40408c78483da9c68bf154f9dcb512f21acb58
Nouman Aziz <m.nouman.ch@gmail.com>
2020-07-25 22:26:06 +0500

    Updates

---
+++ precache-manifest.f957fcdf49597e53b350d554203a7de9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a907e91748b9bfb740dfd08a33af823",
+    "url": "/keep-notes/index.html"
+  },
+  {
+    "revision": "d47ef89b88bb72c3a9b2",
+    "url": "/keep-notes/static/css/main.1f985a59.chunk.css"
+  },
+  {
+    "revision": "869e657964d61c266ad8",
+    "url": "/keep-notes/static/js/2.8b2d9d8b.chunk.js"
+  },
+  {
+    "revision": "5794897cf661d4284a7f0a757ca87b51",
+    "url": "/keep-notes/static/js/2.8b2d9d8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d47ef89b88bb72c3a9b2",
+    "url": "/keep-notes/static/js/main.8abfb68a.chunk.js"
+  },
+  {
+    "revision": "d4aa88e773439ea2d72e",
+    "url": "/keep-notes/static/js/runtime-main.437587ee.js"
+  },
+  {
+    "revision": "d7374de047a72725ae3c116d80f03164",
+    "url": "/keep-notes/static/media/notes.d7374de0.png"
+  }
+]);
